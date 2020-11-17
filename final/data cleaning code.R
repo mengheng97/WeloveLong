ffa27b52451799b82a1cbbea88eb97d293ee308f
Joven-Heng <62068282+Joven-Heng@users.noreply.github.com>
2020-11-17 18:20:18 +0800

    Include datacleaning for the production companies.

--- final/data cleaning code.R
+++ final/data cleaning code.R
@@ -2,10 +2,14 @@
 library(dplyr)
 library(tibble)
 library(lubridate)
+library(readr)
+library(stringr)
 
 # import datasets
-raw_movies <- read.csv("tmdb_5000_movies.csv")
-raw_credits <- read.csv("tmdb_5000_credits.csv")
+raw_movies <- read.csv("tmdb_5000_movies.csv",stringsAsFactors = FALSE)
+raw_credits <- read.csv("tmdb_5000_credits.csv",stringsAsFactors = FALSE)
+top50_companies <- read_csv("top50_companies.csv")
+
 
 # clean movies dataset
 movies <- raw_movies %>% 
@@ -17,5 +21,30 @@ movies <- add_column(movies, month=month(movies$release_date), .before="revenue"
 credits <- raw_credits %>% select(-crew)
 colnames(credits)[1] <- "id"
 
+#Creating the list of names inside each company
+values <- top50_companies$`Production Companies`
+
+
+inTop50<- function(a){
+  company <- a$production_companies
+  #Extracting companies out and putting them in a list
+  names1 <- str_match_all(company, "name\":\\s*(.*?)\\s*\",")[[1]][,2]
+  names1 <- gsub("\"", "", names1)
+  
+  #Checking whether it exists
+  return(ifelse(any(names1%in%values), 1, 0))
+}
+
+for (row in 1:nrow(movies)) {
+  movies$production_companies[row] <- inTop50(movies[row,])
+}
+
+
+#clean productioncrew dataset
+index <- df
+movies$production_companies <- movies$ 
+
 # merge both datasets
 df <- inner_join(movies, credits, by=c("id", "title"))
+
+
